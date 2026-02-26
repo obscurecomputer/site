d110f7b7d02a1ca24a987f5be9d689bbc6b7f32c
gibbiemonster <gibbiemonster@obscure.computer>
2026-02-26 15:16:48 -0700

    fix: review comments

--- src/plugins/extensions/infobox.ts
+++ src/plugins/extensions/infobox.ts
@@ -1,14 +1,28 @@
-import type { TokenizerAndRendererExtension, Tokens } from 'marked';
+/*
+ * Copyright (c) Obscure Computer 2026. Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { TokenizerAndRendererExtension, Tokens } from "marked";
 
 interface AlertToken extends Tokens.Generic {
-  type: 'alert';
+  type: "alert";
   alertType: string;
   text: string;
 }
 
 export const infoboxExtension: TokenizerAndRendererExtension = {
-  name: 'alert',
-  level: 'block',
+  name: "alert",
+  level: "block",
   start(src) { return src.match(/:::(info|note|tip|important|warning|caution)\n/i)?.index; },
   tokenizer(src) {
     const rule = /^:::(info|note|tip|important|warning|caution)\n([\s\S]*?)\n:::\s*(?:\n|$)/i;
@@ -19,7 +33,7 @@ export const infoboxExtension: TokenizerAndRendererExtension = {
       const text = match[2].trim();
 
       return {
-        type: 'alert',
+        type: "alert",
         raw: match[0],
         alertType,
         text,
